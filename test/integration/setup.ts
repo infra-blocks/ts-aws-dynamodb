b5235f1239623c7411aa6e8fd711d87783334333
PhilLavoie <PhilLavoie@users.noreply.github.com>
2025-07-06 07:19:14 -0400

    bootstrapping integration tests
    - Had to lower the mocha version down from 11.7.0 to 11.6.0, since
    the latest update seems to break the import.meta stuff (the ts
    files were interpreted as cjs despite the package.json type). 11.6.0
    works as expected.

--- test/integration/setup.ts
+++ test/integration/setup.ts
@@ -1,7 +1,86 @@
-before("setup", () => {
-  console.log("before all tests hook");
+import * as childProcess from "node:child_process";
+import type { Logger } from "@infra-blocks/logger-interface";
+import { ConsoleLogger } from "@infra-blocks/node-console-logger";
+import type { EnvironmentVariables } from "@infra-blocks/types";
+import { omit } from "radash";
+import { DynamoDbClient } from "../../src/client.js";
+import {
+  getTestConfig,
+  injectTestConfig,
+  type TestConfig,
+} from "./test-config.js";
+
+const CURRENT_DIR = import.meta.dirname;
+
+function runSync(
+  command: string,
+  options: childProcess.ExecSyncOptions & { logger: Logger },
+): void {
+  const { logger } = options;
+  logger.info(`running command: ${command}`);
+  childProcess.execSync(command, {
+    ...omit(options, ["logger"]),
+  });
+}
+
+function dockerComposeUp(params: {
+  env?: EnvironmentVariables;
+  logger: Logger;
+}): void {
+  runSync("docker compose up --build -d", { ...params, cwd: CURRENT_DIR });
+}
+
+function dockerComposeDown(params: { logger: Logger }): void {
+  runSync("docker compose down", { ...params, cwd: CURRENT_DIR });
+}
+
+async function init(params: {
+  config: TestConfig;
+  logger: Logger;
+}): Promise<void> {
+  const { logger, config } = params;
+
+  const { DYNAMODB_ENDPOINT_URL } = config;
+  const dynamoDbPort = new URL("", DYNAMODB_ENDPOINT_URL).port;
+
+  logger.info("starting services");
+  dockerComposeUp({
+    env: {
+      DYNAMODB_PORT: dynamoDbPort,
+      PATH: process.env.PATH,
+    },
+    logger,
+  });
+  const dynamoDbClient = DynamoDbClient.create({
+    dynamodb: { endpoint: DYNAMODB_ENDPOINT_URL },
+    logger,
+  });
+  await dynamoDbClient.ready().on("attempt", ({ attempt }) => {
+    logger.info(`attempt #${attempt} at connecting to DynamoDB`);
+  });
+}
+
+function tearDown(params: { logger: Logger }): void {
+  const { logger } = params;
+  logger.info("stopping services");
+  dockerComposeDown({ logger });
+}
+
+before("test suite setup", async function () {
+  injectTestConfig();
+  const config = getTestConfig();
+  const logger = ConsoleLogger.create({
+    name: "test:integration",
+    level: config.TEST_LOG_LEVEL,
+  });
+  await init({ logger, config });
+
+  this.config = config;
+  this.logger = logger;
+  logger.info("setup complete");
 });
 
-after("tearing down", () => {
-  console.log("after all tests hook");
+after("test suite teardown", function () {
+  tearDown({ logger: this.logger });
+  this.logger.info("teardown complete");
 });
