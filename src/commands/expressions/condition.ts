7cb48e804be22cd9586d09baa38f3eee6a80ab0a
PhilLavoie <PhilLavoie@users.noreply.github.com>
2025-07-12 07:50:30 -0400

    Condition expression functions
    - contains and size. Size introduces a new concept where the function
    needs to be contained within a higher order expression, since it is
    a function that does not return a boolean.
    - contains has been identified as the only function that can also
    wrap the size function. Operators will also have that feature but
    they are non existent at the moment.

--- src/commands/expressions/condition.ts
+++ src/commands/expressions/condition.ts
@@ -1,7 +1,26 @@
-import type { AttributePath, AttributeType } from "../../types.js";
+import type {
+  AttributePath,
+  AttributeType,
+  AttributeValue,
+} from "../../types.js";
 import type { AttributeNames } from "../attributes/names.js";
 import type { AttributeValues } from "../attributes/values.js";
 import type { Expression } from "./expression.js";
+
+export class FunctionExpression implements Expression {
+  private readonly inner: Expression;
+  constructor(params: { inner: Expression }) {
+    const { inner } = params;
+    this.inner = inner;
+  }
+
+  stringify(params: {
+    attributeNames: AttributeNames;
+    attributeValues: AttributeValues;
+  }): string {
+    return this.inner.stringify(params);
+  }
+}
 
 /**
  *
@@ -47,6 +66,10 @@ export class ConditionExpression implements Expression {
   }
 }
 
+/**
+ *
+ * @see https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Expressions.OperatorsAndFunctions.html#Expressions.OperatorsAndFunctions.Functions
+ */
 export function attributeNotExists(
   attribute: AttributePath,
 ): ConditionExpression {
@@ -90,6 +113,35 @@ export function beginsWith(
     inner: {
       stringify: ({ attributeNames, attributeValues }) => {
         return `begins_with(${attributeNames.substitute(attribute)}, ${attributeValues.reference(value)})`;
+      },
+    },
+  });
+}
+
+export function contains(
+  attribute: AttributePath,
+  value: AttributeValue | FunctionExpression,
+): ConditionExpression {
+  return new ConditionExpression({
+    inner: {
+      stringify: ({ attributeNames, attributeValues }) => {
+        if (value instanceof FunctionExpression) {
+          return `contains(${attributeNames.substitute(attribute)}, ${value.stringify({ attributeNames, attributeValues })})`;
+        }
+        return `contains(${attributeNames.substitute(attribute)}, ${attributeValues.reference(value)})`;
+      },
+    },
+  });
+}
+
+// This is the only factory that doesn't return a ConditionExpression. This is because the size function
+// does not return a boolean value, rather a number of items. This also means that it can be used in more
+// complex expressions, such as in a comparison: Toto > size(Tata).
+export function size(attribute: AttributePath): FunctionExpression {
+  return new FunctionExpression({
+    inner: {
+      stringify: ({ attributeNames }) => {
+        return `size(${attributeNames.substitute(attribute)})`;
       },
     },
   });
