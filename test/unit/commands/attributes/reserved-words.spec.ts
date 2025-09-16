a13327362486e6ad8b80a212b280bb09d11f4432
PhilLavoie <PhilLavoie@users.noreply.github.com>
2025-09-16 08:22:32 -0400

    adding reserved word utilities related files
    - This should have been done in the previous merge.

---
+++ test/unit/commands/attributes/reserved-words.spec.ts
@@ -0,0 +1,23 @@
+import { expect } from "@infra-blocks/test";
+import { isReservedWord } from "../../../../src/commands/attributes/reserved-words.js";
+
+describe("commands.attributes.reserved-words", () => {
+  describe(isReservedWord.name, () => {
+    const words: Array<{ expected: boolean; item: string }> = [
+      { expected: true, item: "SELECT" },
+      { expected: true, item: "select" },
+      { expected: true, item: "seLeCt" },
+      { expected: true, item: "FROM" },
+      { expected: true, item: "from" },
+      { expected: true, item: "fRoM" },
+      { expected: false, item: "selects" },
+      { expected: false, item: "form" },
+      { expected: false, item: "regularField" },
+    ];
+    for (const { expected, item } of words) {
+      it(`should return ${expected} for ${item}`, () => {
+        expect(isReservedWord(item)).to.be[expected ? "true" : "false"];
+      });
+    }
+  });
+});
