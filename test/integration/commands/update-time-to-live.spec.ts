f63efd518ee2050f60e5cb344c6711c4e0583d93
PhilLavoie <PhilLavoie@users.noreply.github.com>
2025-12-04 10:38:17 -0500

    update time to live

---
+++ test/integration/commands/update-time-to-live.spec.ts
@@ -0,0 +1,118 @@
+import {
+  DescribeTimeToLiveCommand,
+  DynamoDBClient,
+} from "@aws-sdk/client-dynamodb";
+import { expect } from "@infra-blocks/test";
+import { dropAllTables } from "../fixtures.js";
+
+describe(DynamoDBClient.name, () => {
+  afterEach("clean up", dropAllTables());
+
+  describe("updateTimeToLive", () => {
+    it("should correctly enable it", async function () {
+      const client = this.createClient();
+      const createTableParams = {
+        name: "test-table",
+        primaryKey: {
+          partitionKey: { name: "pk", type: "S" },
+        },
+      } as const;
+      await client.createTable(createTableParams);
+      await client.updateTimeToLive({
+        table: createTableParams.name,
+        attribute: "ttl",
+        enabled: true,
+      });
+      const testClient = this.createTestClient();
+      const response = await testClient.send(
+        new DescribeTimeToLiveCommand({
+          TableName: createTableParams.name,
+        }),
+      );
+      expect(response.TimeToLiveDescription?.AttributeName).to.equal("ttl");
+      expect(["ENABLED", "ENABLING"]).to.include(
+        response.TimeToLiveDescription?.TimeToLiveStatus,
+      );
+    });
+    it("should correctly disable it", async function () {
+      const client = this.createClient();
+      const createTableParams = {
+        name: "test-table",
+        primaryKey: {
+          partitionKey: { name: "pk", type: "S" },
+        },
+      } as const;
+      await client.createTable(createTableParams);
+      await client.updateTimeToLive({
+        table: createTableParams.name,
+        attribute: "ttl",
+        enabled: true,
+      });
+      await client.updateTimeToLive({
+        table: createTableParams.name,
+        attribute: "ttl",
+        enabled: false,
+      });
+      const testClient = this.createTestClient();
+      const response = await testClient.send(
+        new DescribeTimeToLiveCommand({
+          TableName: createTableParams.name,
+        }),
+      );
+      // The attribute isn't part of the response when disabling.
+      expect(response.TimeToLiveDescription?.AttributeName).to.be.undefined;
+      expect(["DISABLED", "DISABLING"]).to.include(
+        response.TimeToLiveDescription?.TimeToLiveStatus,
+      );
+    });
+    it("should fail while disabling already disabled time to live", async function () {
+      const client = this.createClient();
+      const createTableParams = {
+        name: "test-table",
+        primaryKey: {
+          partitionKey: { name: "pk", type: "S" },
+        },
+      } as const;
+      await client.createTable(createTableParams);
+      await expect(
+        client.updateTimeToLive({
+          table: createTableParams.name,
+          attribute: "ttl",
+          enabled: false,
+        }),
+      ).to.be.rejected;
+    });
+    it("should fail while attempting to update on a different attribute", async function () {
+      const client = this.createClient();
+      const createTableParams = {
+        name: "test-table",
+        primaryKey: {
+          partitionKey: { name: "pk", type: "S" },
+        },
+      } as const;
+      await client.createTable(createTableParams);
+      await client.updateTimeToLive({
+        table: createTableParams.name,
+        attribute: "ttl",
+        enabled: true,
+      });
+      await expect(
+        client.updateTimeToLive({
+          table: createTableParams.name,
+          attribute: "cuntdown",
+          enabled: true,
+        }),
+      ).to.be.rejected;
+    });
+    it("should fail if table does not exist", async function () {
+      const client = this.createClient();
+      await expect(
+        client.updateTimeToLive({
+          table: "non-existent",
+          attribute: "ttl",
+          enabled: true,
+        }),
+      ).to.be.rejected;
+    });
+  });
+});
